757ad21f56bbe67c3077565bf49acf3d07bca091
Kaio Fernando <104741718+KaioFernando1993@users.noreply.github.com>
2022-11-22 23:20:11 -0300

    commiti inicial

---
+++ externo.js
@@ -0,0 +1,55 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+
+
+window.onload=function();
+{
+	carrega_nome();
+	document.gertElementybyld('apresentação').onclick=function()[
+	{
+		kaio();
+	}
+
+	document.gertElementybyld('conquistas').onclick=function()
+	{
+		informacao();
+	}
+
+	document.gertElementybyld('redes').onclick=function()
+	{
+		informacao();
+	}
+
+	document.gertElementybyld('fale conosco').onclick=function()
+	{
+		informacao():
+	}
+}
+function informacao()
+{
+var apresentação= prompt('eu sou Kaio Fernando Ferreira Alves, tenho 28 anos sou nascido e criado em Jandira-SP, com tradições de uma familia nordestina venho buscando me desenvolver em uma outra área profissional e que eu venho conhecendo e gostando bastante;');
+var conquistas= pronpt('');
+var redes= pronpt('');
+var fale conosco('tefelefone comercial 12 981207347');
+alert(apresentação+''+conquistas+''redes+''fale conosco;');
+
+}
+function carrega_nome()
+{
+
+
+documentgetElementByld('presentação').value='eu sou Kaio Fernando Ferreira Alves, tenho 28 anos sou nascido e criado em Jandira-SP, com tradições de uma familia nordestina venho buscando me desenvolver em uma outra área profissional e que eu venho conhecendo e gostando bastante; '
+}
+{
+  documentgetElementByld('conquistas').value="";
+}
+{
+	documentgetElementByld('redes').value="";
+}
+{
+	documentgetElementByld('fale conosco').value="tefelefone comercial 12 981207347";
+}
+
